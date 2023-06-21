c0e364e7ab31e8c1ce45a1b9b0d17e71f776e803
Alperen Yilan <alp_eren1616@hotmail.com>
2023-06-21 04:50:12 +0300

    commit on main

---
+++ components/About.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Title from "./ui/Title";
+
+const About = () => {
+  return (
+    <div className='bg-secondary py-14'>
+      <div className='container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse '>
+        <div className='flex justify-center'>
+          <div className='relative sm:w-[445px] sm-h-[600px]  flex justify-center w-[300px] h-[450px]'>
+            <Image src='/images/about-img.png' alt='' layout='fill' />
+          </div>
+        </div>
+        <div className='md:w-1/2'>
+          <Title addClass='text-[40px]'>We Are Feane </Title>
+          <p className='my-5 flex flex-col items-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            deserunt provident ducimus, aliquid ex officiis blanditiis alias
+            cupiditate incidunt ullam debitis dolorum, iste id nam similique
+            distinctio eius aspernatur aliquam.
+          </p>
+          <button className='btn-primary'>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/layout/Footer.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Title from "../ui/Title";
+
+const Footer = () => {
+  return (
+    <div className='bg-secondary text-white'>
+      <div className='container mx-auto pt-16 pb-6'>
+        <div className='flex justify-between text-center'>
+          <div className='md:flex-1'>
+            <Title addClass='text-[30px]'>Contact us</Title>
+            <div className='flex flex-col gap-y-4 mt-6'>
+              <div>
+                <i className='fa fa-map-marker'></i>
+                <span className='inline-block ml-2'>Location</span>
+              </div>
+              <div>
+                <i className='fa fa-phone'></i>
+                <span className='inline-block ml-2'>Call +90 535671</span>
+              </div>
+              <div>
+                <i className='fa fa-envelope'>
+                  <span className='inline-block ml-2'>
+                    alperenyln1@gmail.com
+                  </span>
+                </i>
+              </div>
+            </div>
+          </div>
+          <div className='md:flex-1'>
+            <Title addClass='text-[30px]'>Feane</Title>
+            <p className='mt-3'>
+              Necessary, making this the first true generator on the Internet.
+              It uses a dictionary of over 200 Latin words, combined with
+            </p>
+            <div className='flex items-center justify-center mt-5 gap-x-2'>
+              <a
+                href=''
+                className='w-8 h-8 grid place-content-center bg-white text-secondary rounded-full '
+              >
+                <i className='fa fa-facebook'></i>
+              </a>
+              <a
+                href=''
+                className='w-8 h-8 grid place-content-center bg-white text-secondary rounded-full'
+              >
+                <i className='fa fa-twitter'></i>
+              </a>
+              <a
+                href=''
+                className='w-8 h-8 grid place-content-center bg-white text-secondary rounded-full'
+              >
+                <i className='fa fa-linkedin'></i>
+              </a>
+              <a
+                href=''
+                className='w-8 h-8 grid place-content-center bg-white text-secondary rounded-full'
+              >
+                <i className='fa fa-instagram'></i>
+              </a>
+              <a
+                href=''
+                className='w-8 h-8 grid place-content-center bg-white text-secondary rounded-full'
+              >
+                <i className='fa fa-pinterest'></i>
+              </a>
+            </div>
+          </div>
+          <div className='md:flex-1'>
+            <Title addClass='text-[30px]'>Contact us</Title>
+            <div className='flex flex-col gap-y-4 mt-6'>
+              <div>
+                <i className='fa fa-map-marker'></i>
+                <span className='inline-block ml-2'>Location</span>
+              </div>
+              <div>
+                <i className='fa fa-phone'></i>
+                <span className='inline-block ml-2'>Call +90 535671</span>
+              </div>
+              <div>
+                <i className='fa fa-envelope'>
+                  <span className='inline-block ml-2'>
+                    alperenyln1@gmail.com
+                  </span>
+                </i>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p className='text-center mt-10'> 2022 Templates</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
